a78518af9cebcb9c6dbc718d8eeb965024401ac1
argshook <argshook@gmail.com>
2016-04-28 18:34:24 +0300

    update something
    not finished, pushing so i can work remotely

--- tasks/audio.js
+++ tasks/audio.js
@@ -2,49 +2,23 @@
  * grunt-audio
  * https://github.com/argshook/dotfiles
  *
- * Copyright (c) 2016 argshook
+ * Copyright (c) 2016 Arijus Šukys
  * Licensed under the MIT license.
  */
 
 'use strict';
 
+var Player = require('player');
+
 module.exports = function(grunt) {
+  var music = new Player(__dirname + '/../audio/elevator.mp3');
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
-
-  grunt.registerMultiTask('audio', 'Plugin with task to start playing some mp3 file and a task to stop it. Say, for while tests are running', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
-
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
+  grunt.registerTask('start-audio', function() {
+    return music.play();
   });
 
+  grunt.registerTask('stop-audio', function() {
+    return music.stop();
+  });
 };
+
